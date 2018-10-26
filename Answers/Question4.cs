8aadf0def28f9bc97d444d6f9cc877dfddb62e05
anthoxo <AnthonyNjiva@free.fr>
2018-10-26 19:01:40 +0200

    4

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -1,11 +1,29 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System.Collections.Generic;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
     {
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            result = -1;
+            for (int i = 0 ; i < machineToBeFixed.Rank ; i++) {
+                List<int> tmp = new List<int>();
+                for (int j = 0 ; j < machineToBeFixed.GetLength(i) ; j++) {
+                    if (machineToBeFixed[i, j] == "X") {
+                        tmp.RemoveAll(_ => true);
+                    } else {
+                        tmp.Add(int.Parse(machineToBeFixed[i, j]));
+                        if (tmp.Count == numOfConsecutiveMachines) {
+                            int r = 0;
+                            tmp.ForEach(t => r += t);
+                            result = (result == -1 || r < result) ? r : result;
+                            tmp.RemoveAt(0);
+                        }
+                    }
+                }
+            }
+            return (result == -1) ? 0 : result;
         }
     }
 }
