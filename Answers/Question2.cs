0d162885ae4f5c4e4ef7f3fbb324337c6bd8fa54
anthoxo <AnthonyNjiva@free.fr>
2018-10-26 16:03:18 +0200

    Q2

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -5,7 +5,7 @@
         public static int Answer(int[] cashflowIn, int[] cashflowOut)
         {
             //TODO: Please work out the solution;
-            return -1;
+            return 0;
         }
     }
 }
