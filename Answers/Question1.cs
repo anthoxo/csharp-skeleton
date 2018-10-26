035fa55bb2b0443fae109797cb1cedd32510822c
anthoxo <AnthonyNjiva@free.fr>
2018-10-26 22:47:01 +0200

    1 infzf

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -11,12 +11,14 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         public static int Answer(int[] portfolios)
         {
             //TODO: Please work out the solution;
-            int i;
-            int j;
+            int i,j;
             int answer = 0;
             for (i = 0 ; i < portfolios.Length ; i++) {
                 for (j = i + 1 ; j < portfolios.Length ; j++) {
-                    answer = Math.Max(answer, portfolios[i] ^ portfolios[j]);
+                    int t = portfolios[i] ^ portfolios[j];
+                    if (answer < t){
+                        answer = t;
+                    }
                 }
             }
             return answer;
