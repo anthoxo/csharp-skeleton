0099a4914f3f5fae39146be42b9e5bd3b944f9f7
anthoxo <AnthonyNjiva@free.fr>
2018-10-27 22:32:08 +0200

    5 fzoznf

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -7,67 +7,71 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
+            Console.WriteLine("Ex : " + totalValueOfShares);
             if (numOfShares.Length == 0) {
                 return 0;
             } else {
                 List<int> job = new List<int>();
-                List<int> numbers = new List<int>();
-                List<int> alreadyAdded = new List<int>();
+                SortedList<int, int> numbers = new SortedList<int, int>(new DescendingKeyComparer<int>());
+                SortedList<int, int> alreadyAdded = new SortedList<int, int>(new DescendingKeyComparer<int>());
 
                 int a;
                 int b;
 
                 for (int i = 0 ; i < numOfShares.Length ; i++) {
-                    job.Add(-1);
-                    Question5.AddDesc(numbers, numOfShares[i], false);
+                    job.Add(int.MaxValue - 1);
+                    numbers.Add(numOfShares[i], numOfShares[i]);
                 }
                 for (int i = numOfShares.Length ; i < totalValueOfShares + 1 ; i++) {
-                    job.Add(-1);
+                    job.Add(int.MaxValue - 1);
                 }
                 job[0] = 0;
-                alreadyAdded.Add(0); 
+                alreadyAdded.Add(0, 0); 
                 bool stop = false;
-                for (int i = 0 ; i < numbers.Count && !stop ; i++) {
-                    List<int> tmp = new List<int>();
-                    for (int j = 0 ; j < alreadyAdded.Count; j++) {
-                        for (int k = 1 ; alreadyAdded[j] + numbers[i] * k < totalValueOfShares + 1 ; k++) {
-                            a = numbers[i];
-                            b = alreadyAdded[j];
-                            if (job[b+a*k] == -1 || 1 + job[b+a*(k-1)] < job[a*k]) {
+                for (int i = 0 ; i < numbers.Values.Count && !stop ; i++) {
+                    SortedList<int,int> tmp = new SortedList<int, int>(new DescendingKeyComparer<int>());
+                    for (int j = 0 ; j < alreadyAdded.Values.Count && !stop; j++) {
+                        a = numbers.Values[i];
+                        b = alreadyAdded.Values[j];
+                        for (int k = 1 ; b + a * k < totalValueOfShares + 1 ; k++) {
+                            if (1 + job[b+a*(k-1)] < job[b+a*k]) {
                                 job[b+a*k] = 1 + job[b+a*(k-1)];
-                                Question5.AddDesc(tmp, b+a*k, true);
+                                try {
+                                    tmp.Add(b+a*k,b+a*k);
+                                } catch (System.ArgumentException _) {
+
+                                }
                             }
                             if (b + a * k == totalValueOfShares) {
                                 stop = true;
                             }
                         }
                     }
-                    for (int j = 0 ; j < tmp.Count ; j++) {
-                        Question5.AddDesc(alreadyAdded, tmp[i], true);
+                    // Console.WriteLine("alreadyAdded : " + numbers.Values[i]);
+                    // foreach (KeyValuePair<int, int> kvp in alreadyAdded) {
+                    //     Console.WriteLine(kvp.Key + "," + kvp.Value);
+                    // }
+
+                    // Console.WriteLine("tmp");
+                    // foreach (KeyValuePair<int, int> kvp in tmp) {
+                    //     Console.WriteLine(kvp.Key + "," + kvp.Value);
+                    // }
+                    for (int j = 0 ; j < tmp.Values.Count ; j++) {
+                        try {
+                            alreadyAdded.Add(tmp.Values[j], tmp.Values[j]);
+                        } catch (System.ArgumentException _) {
+
+                        }
+                        
                     }
                 }
-                if (job[totalValueOfShares] == -1) {
+                if (job[totalValueOfShares] == int.MaxValue - 1) {
                     return 0;
                 } else {
                     return job[totalValueOfShares];
                 }
             }
         }
-        public static void AddDesc(List<int> list, int a, bool uniq) {
-            bool pred = true;
-            for (int j = 0; j < list.Count && pred; j++) {
-                if (list[j] <= a) {
-                    pred = false;
-                    if (!uniq || list[j] != a) {
-                        list.Insert(j, a);
-                    }
-                }
-            }
-            if (pred) {
-                list.Add(a);
-            }
-        }
-
     }
 
 }
