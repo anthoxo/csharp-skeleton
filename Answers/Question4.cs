9a88d55ce2016427e755ee9619d536b4c87b5942
anthoxo <AnthonyNjiva@free.fr>
2018-10-28 14:12:13 +0100

    4

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -16,69 +16,75 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         }
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
-            // int result = int.MaxValue - 1;
-            // int sumTmp = 0;
+            if (machineToBeFixed.GetLength(1) < numOfConsecutiveMachines) {
+                return 0;
+            } else {
+                int result = int.MaxValue - 1;
+                int sumTmp = 0;
+                for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
+                    int lastX = -1;
+                    for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
+                        if (machineToBeFixed[i, j] == "X") {
+                            lastX = j;
+                            sumTmp = 0;
+                            if (numOfConsecutiveMachines < j - lastX) {
+                                break;
+                            }
+                        } else {
+                            int a = Question4.Parse(machineToBeFixed[i, j]);
+                            sumTmp += a;
+                            if (j - lastX == numOfConsecutiveMachines) {
+                                if (sumTmp < result) {
+                                    result = sumTmp;
+                                }
+                                lastX += 1;
+                                sumTmp -= Question4.Parse(machineToBeFixed[i, lastX]);
+                                
+                            }
+                        }
+                    }
+                }
+                return (result == int.MaxValue - 1) ? 0 : result;
+            }
+            // List<List<string>> machines = new List<List<string>>();
             // for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
-            //     List<int> tmp = new List<int>();
+            //     List<string> tmp = new List<string>();
             //     for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
-            //         if (machineToBeFixed[i, j] == "X") {
+            //         tmp.Add(machineToBeFixed[i,j]);
+            //     }
+            //     machines.Add(tmp);
+            // }
+            // IEnumerable<int> tt = machines.Select(l => {
+            //     int r = int.MaxValue - 1;
+            //     int sum = 0;
+            //     List<int> tmp = new List<int>();
+            //     for (int i = 0 ; i < l.Count ; i++) {
+            //         if (l[i][0] == 'X') {
             //             tmp.RemoveAll(t => true);
-            //             sumTmp = 0;
-                        
+            //             sum = 0;
             //         } else {
-            //             int a = Question4.Parse(machineToBeFixed[i, j]);
-            //             sumTmp += a;
+            //             int a = Question4.Parse(l[i]);
+            //             sum += a;
             //             tmp.Add(a);
             //             if (tmp.Count == numOfConsecutiveMachines) {
-            //                 if (sumTmp < result) {
-            //                     result = sumTmp;
+            //                 if (sum < r) {
+            //                     r = sum;
             //                 }
-            //                 sumTmp -= tmp[0];
+            //                 sum -= tmp[0];
             //                 tmp.RemoveAt(0);
             //             }
             //         }
             //     }
-            // }
-            // return (result == int.MaxValue - 1) ? 0 : result;
-            List<List<string>> machines = new List<List<string>>();
-            for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
-                List<string> tmp = new List<string>();
-                for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
-                    tmp.Add(machineToBeFixed[i,j]);
-                }
-                machines.Add(tmp);
-            }
-            IEnumerable<int> tt = machines.Select(l => {
-                int r = int.MaxValue - 1;
-                int sum = 0;
-                List<int> tmp = new List<int>();
-                for (int i = 0 ; i < l.Count ; i++) {
-                    if (l[i][0] == 'X') {
-                        tmp.RemoveAll(t => true);
-                        sum = 0;
-                    } else {
-                        int a = Question4.Parse(l[i]);
-                        sum += a;
-                        tmp.Add(a);
-                        if (tmp.Count == numOfConsecutiveMachines) {
-                            if (sum < r) {
-                                r = sum;
-                            }
-                            sum -= tmp[0];
-                            tmp.RemoveAt(0);
-                        }
-                    }
-                }
-                return r;
-            });
-            int result = tt.Aggregate((a,b) => {
-                if (a < b) {
-                    return a;
-                } else {
-                    return b;
-                }
-            });
-            return result;
+            //     return r;
+            // });
+            // int result = tt.Aggregate((a,b) => {
+            //     if (a < b) {
+            //         return a;
+            //     } else {
+            //         return b;
+            //     }
+            // });
+            // return result;
         }
     }
 }
