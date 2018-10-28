a5fb8115894f4484ab9f18de7914e5aa9e20e260
anthoxo <AnthonyNjiva@free.fr>
2018-10-28 04:39:15 +0100

    1

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -12,9 +12,10 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         {
             //TODO: Please work out the solution;
             int i,j;
+            int n = portfolios.Length;
             int answer = 0;
-            for (i = 0 ; i < portfolios.Length ; i++) {
-                for (j = i + 1 ; j < portfolios.Length ; j++) {
+            for (i = 0 ; i < n ; i++) {
+                for (j = i + 1 ; j < n ; j++) {
                     int t = portfolios[i] ^ portfolios[j];
                     if (answer < t){
                         answer = t;
