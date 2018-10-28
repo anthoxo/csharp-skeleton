f910c67810a6ba6da771a4662f062dabd4706748
anthoxo <AnthonyNjiva@free.fr>
2018-10-28 05:39:50 +0100

    4 on verra

--- Answers/Question4.cs
+++ Answers/Question4.cs
@@ -1,36 +1,84 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
     {
+        public static int Parse(string str) {
+            int y = 0;
+            int n = str.Length;
+            for (int i = 0 ; i < n; i++) {
+                y = y * 10 + (str[i] - '0');
+            }
+            return y;
+        }
         public static int Answer(string[,] machineToBeFixed, int numOfConsecutiveMachines)
         {
-            int result = int.MaxValue - 1;
-            int sumTmp = 0;
-            for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
-                List<int> tmp = new List<int>();
-                for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
-                    if (machineToBeFixed[i, j] == "X") {
-                        tmp.RemoveAll(t => true);
-                        sumTmp = 0;
+            // int result = int.MaxValue - 1;
+            // int sumTmp = 0;
+            // for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
+            //     List<int> tmp = new List<int>();
+            //     for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
+            //         if (machineToBeFixed[i, j] == "X") {
+            //             tmp.RemoveAll(t => true);
+            //             sumTmp = 0;
                         
+            //         } else {
+            //             int a = Question4.Parse(machineToBeFixed[i, j]);
+            //             sumTmp += a;
+            //             tmp.Add(a);
+            //             if (tmp.Count == numOfConsecutiveMachines) {
+            //                 if (sumTmp < result) {
+            //                     result = sumTmp;
+            //                 }
+            //                 sumTmp -= tmp[0];
+            //                 tmp.RemoveAt(0);
+            //             }
+            //         }
+            //     }
+            // }
+            // return (result == int.MaxValue - 1) ? 0 : result;
+            List<List<string>> machines = new List<List<string>>();
+            for (int i = 0 ; i < machineToBeFixed.GetLength(0) ; i++) {
+                List<string> tmp = new List<string>();
+                for (int j = 0 ; j < machineToBeFixed.GetLength(1) ; j++) {
+                    tmp.Add(machineToBeFixed[i,j]);
+                }
+                machines.Add(tmp);
+            }
+            IEnumerable<int> tt = machines.Select(l => {
+                int r = int.MaxValue - 1;
+                int sum = 0;
+                List<int> tmp = new List<int>();
+                for (int i = 0 ; i < l.Count ; i++) {
+                    if (l[i][0] == 'X') {
+                        tmp.RemoveAll(t => true);
+                        sum = 0;
                     } else {
-                        int a = int.Parse(machineToBeFixed[i, j]);
-                        sumTmp += a;
+                        int a = Question4.Parse(l[i]);
+                        sum += a;
                         tmp.Add(a);
                         if (tmp.Count == numOfConsecutiveMachines) {
-                            if (sumTmp < result) {
-                                result = sumTmp;
+                            if (sum < r) {
+                                r = sum;
                             }
-                            sumTmp -= tmp[0];
+                            sum -= tmp[0];
                             tmp.RemoveAt(0);
                         }
                     }
                 }
-            }
-            return (result == int.MaxValue - 1) ? 0 : result;
+                return r;
+            });
+            int result = tt.Aggregate((a,b) => {
+                if (a < b) {
+                    return a;
+                } else {
+                    return b;
+                }
+            });
+            return result;
         }
     }
 }
