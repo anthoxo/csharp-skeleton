8314c1684e5b5d69e0f5e19a34c7f696a82507c0
anthoxo <AnthonyNjiva@free.fr>
2018-10-28 17:30:08 +0100

    5 brut force

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -14,7 +14,6 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 SortedSet<int> numbers = new SortedSet<int>(new DescendingKeyComparer<int>());
                 SortedSet<int> alreadyAdded = new SortedSet<int>(new DescendingKeyComparer<int>());
 
-                List<int> tmpNumbers = new List<int>();
                 int a;
                 int b;
 
@@ -30,25 +29,34 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 job[0] = 0;
                 alreadyAdded.Add(0);
                 foreach(int number in numbers) {
-                    List<int> tmp = new List<int>();
-                    foreach (int added in alreadyAdded) {
-                        a = number;
-                        b = added;
+                    // List<int> tmp = new List<int>();
+                    // foreach (int added in alreadyAdded) {
+                    //     a = number;
+                    //     b = added;
                         
-                        for (int k = a ; b + k < totalValueOfShares + 1 ; k = k + a) {
-                            int c = b + k;
-                            if (1 + job[c-a] < job[c] || job[c] == -1) {
-                                job[c] = 1 + job[c-a];
-                                tmp.Add(c);
+                    //     for (int k = a ; b + k < totalValueOfShares + 1 ; k = k + a) {
+                    //         int c = b + k;
+                    //         if (1 + job[c-a] < job[c] || job[c] == -1) {
+                    //             job[c] = 1 + job[c-a];
+                    //             tmp.Add(c);
+                    //         }
+                    //     }
+                    // }
+                    // foreach (int tmpValue in tmp) {
+                    //     try {
+                    //         alreadyAdded.Add(tmpValue);
+                    //     } catch (System.ArgumentException) {
+
+                    //     } 
+                    // }
+                    for (int k = 1 ; k < totalValueOfShares + 1 ; k++) {
+                        if (number <= k) {
+                            if (job[k - number] != -1) {
+                                if (1 + job[k - number] < job[k] || job[k] == -1) {
+                                    job[k] = 1 + job[k - number];
+                                }
                             }
                         }
-                    }
-                    foreach (int tmpValue in tmp) {
-                        try {
-                            alreadyAdded.Add(tmpValue);
-                        } catch (System.ArgumentException) {
-
-                        } 
                     }
                 }
                 if (job[totalValueOfShares] == - 1) {
