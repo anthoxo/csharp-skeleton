b6f0996869259f2abc3a2f2fa7eb7e1839ba23ca
anthoxo <AnthonyNjiva@free.fr>
2018-10-29 05:50:15 +0100

    if

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -6,101 +6,63 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         class Solution {
 
-            public List<List<int>> inList;
-            public List<List<int>> outList;
+            public Dictionary<int, int> dicoIn;
+            public Dictionary<int, int> dicoOut;
             public void GeneratePartitions(int[] cashflowIn, int[] cashflowOut) {
-                this.inList = new List<List<int>>();
-                this.outList = new List<List<int>>();
-
-                List<int> tmpIn = new List<int>();
-                List<int> tmpOut = new List<int>();
+                this.dicoIn = new Dictionary<int, int>();
+                this.dicoOut = new Dictionary<int, int>();
                 int m1 = (cashflowIn.Length < cashflowOut.Length) ? cashflowIn.Length : cashflowOut.Length;
                 int m2 = (m1 == cashflowOut.Length) ? cashflowIn.Length : cashflowOut.Length;
                 for (int i = 0 ; i < m1 ; i++) {
-                    tmpIn.Add(cashflowIn[i]);
-                    tmpOut.Add(cashflowOut[i]);
+                    int t1 = cashflowIn[i];
+                    int t2 = cashflowOut[i];
+                    if (!dicoIn.TryAdd(t1, 1)) {
+                        dicoIn[t1] += 1; 
+                    }
+                    if (!dicoOut.TryAdd(t2, 1)) {
+                        dicoOut[t2] += 1; 
+                    }
                 }
                 if (m1 == cashflowOut.Length) {
                     for (int i = m1 ; i < m2 ; i++) {
-                        tmpIn.Add(cashflowIn[i]);
+                        int t1 = cashflowIn[i];
+                        if (!dicoIn.TryAdd(t1, 1)) {
+                            dicoIn[t1] += 1; 
+                        }
                     }
                 } else {
                     for (int i = m1 ; i < m2 ; i++) {
-                        tmpOut.Add(cashflowOut[i]);
-                    }
-                }
-                tmpIn.Sort();
-                tmpOut.Sort();
-                int a = tmpIn[0];
-                int tmpA = 0;
-                List<int> tmp = new List<int>();
-                while (tmpIn.Count != 0) {
-                    if (a == tmpIn[0]) {
-                        tmpA += a;
-                    } else {
-                        this.inList.Add(tmp);
-                        tmp = new List<int>();
-                        a = tmpIn[0];
-                        tmpA = a;
-                    }
-                    tmp.Add(tmpA);
-                    tmpIn.RemoveAt(0);
-                }
-                this.inList.Add(tmp);
-                a = tmpOut[0];
-                tmpA = 0;
-                tmp = new List<int>();
-                while (tmpOut.Count != 0) {
-                    if (a == tmpOut[0]) {
-                        tmpA += a;
-                    } else {
-                        outList.Add(tmp);
-                        tmp = new List<int>();
-                        a = tmpOut[0];
-                        tmpA = a;
-                    }
-                    tmp.Add(tmpA);
-                    tmpOut.RemoveAt(0);
-                }
-                outList.Add(tmp);
-            }
-
-            public SortedSet<int> FindAllSum(List<List<int>> countList) {
-                SortedSet<int> alreadyAdded = new SortedSet<int>();
-                for (int i = 0 ; i < countList.Count ; i++) {
-                    List<int> count = countList[i];
-                    List<int> tmp = new List<int>();
-                    for (int j = 0 ; j < count.Count ; j++) {
-                        tmp.Add(count[j]);
-                        foreach (int kk in alreadyAdded) {
-                            tmp.Add(count[j] + kk);
+                    int t2 = cashflowOut[i];
+                        if (!dicoOut.TryAdd(t2, 1)) {
+                            dicoOut[t2] += 1; 
                         }
                     }
-                    for (int j = 0 ; j < tmp.Count ; j++) {
-                        try {
-                            alreadyAdded.Add(tmp[j]);
-                        } catch (System.ArgumentException) {
-
-                        } 
-                    }
                 }
-                return alreadyAdded;
+            }
+
+            public SortedSet<int> FindAllSum(Dictionary<int,int> dico) {
+                List<int> alreadyAdded = new List<int>();
+                foreach (KeyValuePair<int,int> kvp in dico) {
+                    List<int> tmp = new List<int>();
+                    int a = kvp.Key;
+                    for (int i = 0 ; i < kvp.Value ; i++) {
+                        foreach (int kk in alreadyAdded) {
+                            tmp.Add(kk + a);
+                            a += kvp.Key;
+                        }
+                    }
+                    a = kvp.Key;
+                    for (int i = 0 ; i < kvp.Value ; i++) {
+                        tmp.Add(a);
+                        a += kvp.Key;
+                    }
+                    alreadyAdded.AddRange(tmp);
+                }
+                return new SortedSet<int>(alreadyAdded);
             }
 
             public int FindMinDistance(SortedSet<int> l1, SortedSet<int> l2) {
                 int min = 1000;
-                // foreach (int a1 in l1) {
-                //     foreach (int a2 in l2) {
-                //         if (a1 - a2 == 0) {
-                //             return 0;
-                //         } else {
-                //             int t = Math.Abs(a1-a2);
-                //             if (t < min) {
-                //                 min = t;
-                //             }
-                //         }
-                //     }
-                // }
                 foreach (int a1 in l1) {
                     int tmpMin = int.MaxValue;
                     int increase = -1;
@@ -125,7 +87,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 
             public int Run(int[] cashflowIn, int[] cashflowOut) {
                 this.GeneratePartitions(cashflowIn, cashflowOut);
-                return this.FindMinDistance(this.FindAllSum(this.inList), this.FindAllSum(this.outList));
+                return this.FindMinDistance(this.FindAllSum(this.dicoIn), this.FindAllSum(this.dicoOut));
             }
         }
         public static int Answer(int[] cashflowIn, int[] cashflowOut)
