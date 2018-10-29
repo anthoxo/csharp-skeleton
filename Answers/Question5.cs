4cc78076787696ae2d193c944bc4db11d22a428c
anthoxo <AnthonyNjiva@free.fr>
2018-10-29 14:53:49 +0100

    5

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -11,35 +11,38 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             } else {
                 List<int> job = new List<int>();
                 List<int> numbers = new List<int>();
-                List<int> alreadyAdded = new List<int>();
+                List<bool> alreadyAdded = new List<bool>();
 
                 int a;
                 int b;
 
                 for (int i = 0 ; i < numOfShares.Length ; i++) {
                     job.Add(int.MaxValue);
+                    alreadyAdded.Add(false);
                     if (numOfShares[i] > 0) {
                         numbers.Add(numOfShares[i]);
                     }
                 }
                 for (int i = numOfShares.Length ; i < totalValueOfShares + 1 ; i++) {
                     job.Add(int.MaxValue);
+                    alreadyAdded.Add(false);
                 }
                 job[0] = 0;
-                alreadyAdded.Add(0);
+                alreadyAdded[0] = true;
                 foreach(int number in numbers) {
-                    List<int> tmp = new List<int>(alreadyAdded);
-                    foreach (int added in tmp) {
-                        a = number;
-                        b = added;
-                        
-                        for (int k = a ; b + k < totalValueOfShares + 1 ; k = k + a) {
-                            int c = b + k;
-                            int t = job[c-a];
-                            if (1 + t < job[c]) {
-                                job[c] = 1 + t;
-                                alreadyAdded.Add(c);
+                    for (int i = 0 ; i < totalValueOfShares + 1 ; i++) {
+                        if (alreadyAdded[i]) {
+                            a = number;
+                            b = i;
+                            for (int k = a ; b + k < totalValueOfShares + 1 ; k = k + a) {
+                                int c = b + k;
+                                int t = job[c-a];
+                                if (1 + t < job[c]) {
+                                    job[c] = 1 + t;
+                                    alreadyAdded[c] = true;
+                                }
                             }
+
                         }
                     }
                 }
@@ -55,30 +58,36 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             if (numOfShares.Length == 0) {
                 return 0;
             } else {
-                List<int> job = new List<int>();
+                int max = 10000;
+                List<int> job = new List<int>(totalValueOfShares + 1);
                 for (int i = 0 ; i < totalValueOfShares + 1 ; i++) {
-                    job.Add(10000);
+                    job.Add(max);
                 }
                 job[0] = 0;
                 int n = numOfShares.Length;
                 for (int i = 0 ; i < n ; i++) {
                     int number = numOfShares[i];
                     if (number > 0) {
-                        for (int k = 1 ; k < totalValueOfShares + 1 ; k++) {
-                            if (number <= k) {
-                                if (1 + job[k - number] < job[k]) {
-                                    job[k] = 1 + job[k - number];
-                                }
+                        for (int k = number ; k < totalValueOfShares + 1 ; k++) {
+                            int r = job[k - number] + 1;
+                            if (r < job[k]) {
+                                job[k] = r;
                             }
                         }
                     }
                 }
-                if (job[totalValueOfShares] == 10000) {
+                int t = job[totalValueOfShares];
+                if (t == max) {
                     return 0;
                 } else {
-                    return job[totalValueOfShares];
+                    return t;
                 }
             }
+        }
+
+        public static int Answer3(int[] numOfShares, int totalValueOfShares)
+        {
+            return 0;
         }
     }
 
