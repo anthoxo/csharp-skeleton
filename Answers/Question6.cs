54869225356ead3677138510036fa5a2e26e3eb6
anthoxo <AnthonyNjiva@free.fr>
2018-10-30 22:49:59 +0100

    6

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -39,7 +39,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         }
     }
 
-    class Dijkstra {
+    class Dijkstra2 {
         public Graph graph;
         bool[] alreadySeen;
         int[] distance;
@@ -47,7 +47,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         int indiceMin;
         int valueIndiceMin;
         
-        public Dijkstra(Graph graph, int finalNode) {
+        public Dijkstra2(Graph graph, int finalNode) {
             this.graph = graph;
             this.alreadySeen = new bool[this.graph.nbNodes];
             this.distance = new int[this.graph.nbNodes];
@@ -107,6 +107,76 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             return this.distance[ind];
         }
     }
+    class Dijkstra {
+        public Graph graph;
+        bool[] alreadySeen;
+        int[] distance;
+        int finalNode;
+        int indiceMin;
+        int valueIndiceMin;
+        
+        public Dijkstra(Graph graph, int finalNode) {
+            this.graph = graph;
+            this.alreadySeen = new bool[this.graph.nbNodes];
+            this.distance = new int[this.graph.nbNodes];
+            this.finalNode = finalNode;
+            this.indiceMin = - 1;
+            this.InitValues();
+        }
+        public void InitValues() {
+            this.distance[0] = 0;
+            this.alreadySeen[0] = false;
+            this.indiceMin = 0;
+            this.valueIndiceMin = 0;
+            for (int i = 1 ; i < this.graph.nbNodes ; i++) {
+                this.distance[i] = int.MaxValue;
+                this.alreadySeen[i] = false;
+            }
+        }
+        public int FindIndexMin() {
+            return this.indiceMin;
+        }
+
+        public int majDistance(int s1, int s2, int d1, int d2) {
+            if (d2 > d1) {
+                this.distance[s2] = d1;
+                return d1;
+            } else {
+                return d2;
+            }
+        }
+
+        public int StepDijkstra() {
+            int ind = this.FindIndexMin();
+            this.alreadySeen[ind] = true;
+            if (ind != this.finalNode) {
+                this.indiceMin = this.finalNode;
+                this.valueIndiceMin = this.distance[this.finalNode];
+                int i = 0;
+                foreach(bool v in this.alreadySeen) {
+                    if (!v) {
+                        int edge = this.graph.GetEdge(ind, i);
+                        int d2 = this.distance[i];
+                        int d1 = this.distance[ind] + edge;
+                        d2 = this.majDistance(ind, i, d1, d2);
+                        if (d2 < this.valueIndiceMin) {
+                            this.indiceMin = i;
+                            this.valueIndiceMin = d2;
+                        }
+                    }
+                    i++;
+                }
+            }
+            return ind;
+        }
+        public int StartDijkstra() {
+            int ind = 0;
+            while(ind != this.finalNode) {
+                ind = this.StepDijkstra();
+            }
+            return this.distance[ind];
+        }
+    }
 
     public class Question6
     {
@@ -118,6 +188,17 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
             Graph g = new Graph(numOfServers, connectionTimeMatrix);
             Dijkstra dijkstra = new Dijkstra(g, targetServer);
+            int result = dijkstra.StartDijkstra();
+            return result;
+        }
+        public static int Answer2(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
+        {
+            //TODO: Please work out the solution;
+            if (numOfServers < 2 || targetServer == 0) {
+                return 0;
+            }
+            Graph g = new Graph(numOfServers, connectionTimeMatrix);
+            Dijkstra2 dijkstra = new Dijkstra2(g, targetServer);
             int result = dijkstra.StartDijkstra();
             return result;
         }
