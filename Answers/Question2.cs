da5d8892c4dc037718e3371669c1e43a86a1f252
anthoxo <AnthonyNjiva@free.fr>
2018-10-31 18:08:13 +0100

    2

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -4,7 +4,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
-        class Solution {
+        class Solution2 {
 
             public Dictionary<int, int> dicoIn;
             public Dictionary<int, int> dicoOut;
@@ -49,9 +49,6 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                         foreach (int kk in alreadyAdded) {
                             tmp.Add(kk + a);
                         }
-                    }
-                    a = kvp.Key;
-                    for (int i = 0 ; i < kvp.Value ; i++) {
                         tmp.Add(a);
                         a += kvp.Key;
                     }
@@ -60,37 +57,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 alreadyAdded.Sort();
                 return alreadyAdded;
             }
-
-            public int FindMinDistance(List<int> l1, List<int> l2) {
-                int min = (l1[0] < l2[0]) ? l1[0] : l2[0];
-                int i = 0;
-                int j = 0;
-                int n = l1.Count;
-                int m = l2.Count;
-                while (i < n && j < m) {
-                    int a1 = l1[i];
-                    int a2 = l2[j];
-                    int t;
-                    if (a1 < a2) {
-                        i++;
-                        t = a2 - a1;
-                        if (t < min) {
-                            min = t;
-                        }
-                    } else {
-                        j++;
-                        t = a1 - a2;
-                        if (t < min) {
-                            min = t;
-                        }
-                    }
-                    if (min == 0) {
-                        return 0;
-                    }
-                }
-                return min;
-            }
-            public int FindMinDistance2(int[] l1, int[] l2) {
+            public int FindMinDistance(int[] l1, int[] l2) {
                 int min = (l1[0] < l2[0]) ? l1[0] : l2[0];
                 int i = 0;
                 int j = 0;
@@ -101,13 +68,13 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                     int a2 = l2[j];
                     int t;
                     if (a1 < a2) {
-                        i++;
+                        i += 1;
                         t = a2 - a1;
                         if (t < min) {
                             min = t;
                         }
                     } else {
-                        j++;
+                        j += 1;
                         t = a1 - a2;
                         if (t < min) {
                             min = t;
@@ -122,21 +89,83 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 
             public int Run(int[] cashflowIn, int[] cashflowOut) {
                 this.GeneratePartitions(cashflowIn, cashflowOut);
-                return this.FindMinDistance(this.FindAllSum(this.dicoIn), this.FindAllSum(this.dicoOut));
+                return this.FindMinDistance(this.FindAllSum(this.dicoIn).ToArray(), this.FindAllSum(this.dicoOut).ToArray());
             }
-
-            public int Run2(int[] cashflowIn, int[] cashflowOut) {
-                this.GeneratePartitions(cashflowIn, cashflowOut);
-                return this.FindMinDistance2(this.FindAllSum(this.dicoIn).ToArray(), this.FindAllSum(this.dicoOut).ToArray());
-            }
-
-
             public int GetMin(int[] tab) {
                 int r = int.MaxValue;
                 for (int i = 0 ; i < tab.Length ; i++) {
                     int t = tab[i];
                     if (t < r) {
                         r = t;
+                    }
+                }
+                return r;
+            }
+        }
+        class Solution {
+
+            public int[] dicoIn;
+            public int[] dicoOut;
+            public void GeneratePartitions(int[] cashflowIn, int[] cashflowOut) {
+                this.dicoIn = cashflowIn;
+                this.dicoOut = cashflowOut;
+            }
+            public List<int> FindAllSum(int[] dico) {
+                int[] alreadyAdded = new int[(int)Math.Pow(2, dico.Length)];
+                int i = 0;
+                int j = 0;
+                foreach (int v in dico) {
+                    for (int kk = 0 ; kk < i ; ++kk) {
+                        alreadyAdded[j + kk] = kk + v;
+                        j += 1;
+                    }
+                    i = j;
+                    alreadyAdded[i] = v;
+                    j += 1;
+                }
+                List<int> result = new List<int>(alreadyAdded);
+                result.Sort();
+                return result;
+            }
+            public int FindMinDistance(int[] l1, int[] l2) {
+                int min = (l1[0] < l2[0]) ? l1[0] : l2[0];
+                int i = 0;
+                int j = 0;
+                int n = l1.Length;
+                int m = l2.Length;
+                while (i < n && j < m) {
+                    int a1 = l1[i];
+                    int a2 = l2[j];
+                    int t;
+                    if (a1 < a2) {
+                        i += 1;
+                        t = a2 - a1;
+                        if (t < min) {
+                            min = t;
+                        }
+                    } else {
+                        j += 1;
+                        t = a1 - a2;
+                        if (t < min) {
+                            min = t;
+                        }
+                    }
+                    if (min == 0) {
+                        return 0;
+                    }
+                }
+                return min;
+            }
+
+            public int Run(int[] cashflowIn, int[] cashflowOut) {
+                this.GeneratePartitions(cashflowIn, cashflowOut);
+                return this.FindMinDistance(this.FindAllSum(this.dicoIn).ToArray(), this.FindAllSum(this.dicoOut).ToArray());
+            }
+            public int GetMin(int[] tab) {
+                int r = int.MaxValue;
+                foreach (int v in tab) {
+                    if (v < r) {
+                        r = v;
                     }
                 }
                 return r;
@@ -156,15 +185,26 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 
         public static int Answer2(int[] cashflowIn, int[] cashflowOut)
         {            
-            Solution s = new Solution();
+            Solution2 s = new Solution2();
             if (cashflowIn.Length == 0) {
                 return s.GetMin(cashflowOut);
             } else if (cashflowOut.Length == 0) {
                 return s.GetMin(cashflowIn);
             } else {
-                return s.Run2(cashflowIn, cashflowOut);
+                return s.Run(cashflowIn, cashflowOut);
             }
         }
 
     }
 }
+
+/**
+JE LAISSE EN SUSPEND / LES DEUX ALGOS SEMBLENT FAIRE LE MEME TEMPS
+ */
+
+//  public static async System.Threading.Tasks.Task<int> AnswerRegroupParraTask(int[] cashflowIn, int[] cashflowOut) {
+//         int degreeOfParallelism = Environment.ProcessorCount;
+//         if(Environment.ProcessorCount > 1){
+//             var results = await System.Threading.Tasks.Task.WhenAll(
+//                 System.Threading.Tasks.Task.Run(() => sumSetCounts(cashflowIn)),
+//                 System.Threading.Tasks.Task.Run(() => sumSetCounts(cashflowOut)));
