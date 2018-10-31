23258f6a1bb1d777fd956edc55d75286b34720ee
anthoxo <AnthonyNjiva@free.fr>
2018-10-31 14:48:37 +0100

    5

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
@@ -53,30 +54,30 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         //         }
         //     }
         // }
-        public static int Answer(int[] numOfShares, int totalValueOfShares)
+        public static int Answer2(int[] numOfShares, int totalValueOfShares)
         {
             if (numOfShares.Length == 0 || totalValueOfShares == 0) {
                 return 0;
             } else {
                 int max = 10000;
-                int[] job = new int[totalValueOfShares + 1];
+                int[] bruh = new int[totalValueOfShares + 1];
                 for (int i = 0 ; i < totalValueOfShares + 1 ; i += 1) {
-                    job[i] = max;
+                    bruh[i] = max;
                 }
-                job[0] = 0;
+                bruh[0] = 0;
                 int n = numOfShares.Length;
                 for (int i = 0 ; i < n ; i++) {
                     int number = numOfShares[i];
                     if (number > 0) {
                         for (int k = number ; k < totalValueOfShares + 1 ; k += 1) {
-                            int r = job[k - number] + 1;
-                            if (r < job[k]) {
-                                job[k] = r;
+                            int r = bruh[k - number] + 1;
+                            if (r < bruh[k]) {
+                                bruh[k] = r;
                             }
                         }
                     }
                 }
-                int t = job[totalValueOfShares];
+                int t = bruh[totalValueOfShares];
                 if (t == max) {
                     return 0;
                 } else {
@@ -84,37 +85,38 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 }
             }
         }
-        public static int Answer2(int[] numOfShares, int totalValueOfShares)
+        public static int Answer(int[] numOfShares, int totalValueOfShares)
         {
-            if (numOfShares.Length == 0 || totalValueOfShares == 0) {
+            int max = numOfShares.Sum();
+            if (max < totalValueOfShares) {
                 return 0;
             } else {
-                int max = 10000;
-                int[] job = new int[totalValueOfShares + 1];
-                int i = 0;
-                foreach(int v in job) {
-                    job[i] = max;
-                    i += 1;
+                int max1 = totalValueOfShares+1;
+                int[] bruh = new int[totalValueOfShares + 1];
+                for (int i = 0 ; i < totalValueOfShares + 1 ; i += 1) {
+                    bruh[i] = max1;
                 }
-                job[0] = 0;
-                
-                int n = numOfShares.Length;
-                foreach(int number in numOfShares) {
-                    if (number > 0) {
-                        for (int k = number ; k < totalValueOfShares + 1 ; k++) {
-                            int r = job[k - number] + 1;
-                            if (r < job[k]) {
-                                job[k] = r;
+                bruh[0] = 0;
+                List<int> alreadyAdded = new List<int>();
+                List<int> tmp = new List<int>();
+                foreach (int num in numOfShares) {
+                    int n = totalValueOfShares/num;
+                    for (int i = num ; i < n ; i += num) {
+                        foreach (int poop in alreadyAdded) {
+                            int t = poop + i;
+                            if (t < totalValueOfShares + 1) {
+                                tmp.Add(poop + i);
+                                int r = bruh[i - num] + 1;
+                                if (r < bruh[i]) {
+                                    bruh[i] = r;
+                                }
                             }
                         }
+                        tmp.Add(i);
                     }
+                    alreadyAdded.AddRange(tmp);
                 }
-                int t = job[totalValueOfShares];
-                if (t == max) {
-                    return 0;
-                } else {
-                    return t;
-                }
+                return (bruh[totalValueOfShares] == max1) ? 0 : bruh[totalValueOfShares];
             }
         }
     }
