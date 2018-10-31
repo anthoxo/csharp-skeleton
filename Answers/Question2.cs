45ca956819215ad6e5910eb7872809ed25ae9b61
anthoxo <AnthonyNjiva@free.fr>
2018-10-31 20:07:55 +0100

    3

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
+using System.Linq;
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
@@ -113,19 +114,19 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 this.dicoOut = cashflowOut;
             }
             public List<int> FindAllSum(int[] dico) {
-                int[] alreadyAdded = new int[(int)Math.Pow(2, dico.Length)];
+                int[] sums = new int[(int)Math.Pow(2, dico.Length) - 1];
                 int i = 0;
                 int j = 0;
                 foreach (int v in dico) {
+                    i = j;
                     for (int kk = 0 ; kk < i ; ++kk) {
-                        alreadyAdded[j + kk] = kk + v;
+                        sums[i + kk] = sums[kk] + v;
                         j += 1;
                     }
-                    i = j;
-                    alreadyAdded[i] = v;
+                    sums[j] = v;
                     j += 1;
                 }
-                List<int> result = new List<int>(alreadyAdded);
+                List<int> result = new List<int>(sums);
                 result.Sort();
                 return result;
             }
@@ -173,7 +174,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
                 return r;
             }
         }
-        public static int Answer2(int[] cashflowIn, int[] cashflowOut)
+        public static int Answer(int[] cashflowIn, int[] cashflowOut)
         {            
             Solution s = new Solution();
             if (cashflowIn.Length == 0) {
@@ -185,7 +186,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             }
         }
 
-        public static int Answer(int[] cashflowIn, int[] cashflowOut)
+        public static int Answer2(int[] cashflowIn, int[] cashflowOut)
         {            
             Solution2 s = new Solution2();
             if (cashflowIn.Length == 0) {
@@ -198,6 +199,75 @@ namespace C_Sharp_Challenge_Skeleton.Answers
         }
 
     }
+        public class Question2bis
+    {
+        public static int findMinDist(int[] cashIn, int[] cashOut){
+            int n = cashIn.Length;
+            int m = cashOut.Length;
+            int minDist = -1;
+            // there should at least be a zero in each list
+            if(n==1 && m == 1) return -1;
+            else if(n==1) minDist = cashOut[1];
+            else if(m==1) minDist = cashIn[1];
+            else minDist = cashIn[1] < cashOut[1] ? cashIn[1] : cashOut[1];
+            int i = 1;
+            int j = 1;
+            int dist;
+            while(true){
+                dist = Math.Abs(cashIn[i]-cashOut[j]);
+                
+                if(dist == 0)
+                    return 0;
+                else if(dist < minDist) 
+                    minDist = dist;
+
+                if(cashIn[i] < cashOut[j] && i != n-1)
+                    i += 1;
+                else if(cashIn[i] >= cashOut[j] && j != m-1)
+                    j += 1;
+                else
+                    // we're either at the end of both lists or gonna increase the gap between values
+                    break;
+            }
+            return minDist;
+        }
+        public static int[] sumSetCust2(int[] cashFlow) {
+            int n = cashFlow.Length;
+            int maxSum = 0;
+            for(int i=0; i<n; ++i)
+                maxSum += cashFlow[i];
+
+            bool[] sumSetIdx = new bool[maxSum+1];
+            sumSetIdx[0] = true;
+
+            List<int> sumSet = new List<int>();
+            sumSet.Add(0);
+            for(int i=0; i<n; ++i){
+                List<int> newSumSet = new List<int>();
+                int lenSumSet = sumSet.Count();
+                int cashFlowI = cashFlow[i];
+                for(int j=0; j<lenSumSet; j++){
+                    int newSum = sumSet[j]+cashFlowI;
+                    if(!sumSetIdx[newSum]){
+                        sumSetIdx[newSum] = true;
+                        newSumSet.Add(newSum);
+                    }
+                }
+                for(int j=0; j<newSumSet.Count(); j++){
+                    sumSet.Add(newSumSet[j]);
+                }
+            }
+            sumSet.Sort();
+            return sumSet.ToArray();
+        }
+        public static int Answer(int[] cashflowIn, int[] cashflowOut) {
+            int[] cashIn = sumSetCust2(cashflowIn);
+            int[] cashOut = sumSetCust2(cashflowOut);
+            int minDist = findMinDist(cashIn, cashOut);
+            return minDist;
+        }
+    }
+
 }
 
 /**
