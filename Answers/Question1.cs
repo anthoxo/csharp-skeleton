8a62d5eb04fe2ccb77c6405b31ec905673b0c2f7
anthoxo <AnthonyNjiva@free.fr>
2018-10-31 15:16:59 +0100

    1 bis

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -8,7 +8,7 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question1
     {
-        public static int Answer2(int[] portfolios)
+        public static int Answer(int[] portfolios)
         {
             int n = portfolios.Length;
             if (n < 2) {
@@ -16,20 +16,38 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             } else {
                 int i,j;
                 int answer = 0;
+                int zbeub = n - 4;
                 i = 0;
                 foreach(int p in portfolios) {
-                    for (j = i + 1 ; j < n ; j += 1) {
-                        int t = p ^ portfolios[j];
-                        if (answer < t){
-                            answer = t;
+                    for (j = i + 1 ; j < zbeub ; j += 5) {
+                        if (answer < (p ^ portfolios[j])){
+                            answer = p ^ portfolios[j];
                         }
+                        if (answer < (p ^ portfolios[j+1])){
+                            answer = p ^ portfolios[j+1];
+                        }
+                        if (answer < (p ^ portfolios[j+2])){
+                            answer = p ^ portfolios[j+2];
+                        }
+                        if (answer < (p ^ portfolios[j+3])){
+                            answer = p ^ portfolios[j+3];
+                        }
+                        if (answer < (p ^ portfolios[j+4])){
+                            answer = p ^ portfolios[j+4];
+                        }
+                    }
+                    while (j < n) {
+                        if (answer < (p ^ portfolios[j])){
+                            answer = p ^ portfolios[j];
+                        }
+                        j += 1;
                     }
                     i += 1;
                 }
                 return answer;
             }
         }
-        public static int Answer(int[] portfolios)
+        public static int Answer2(int[] portfolios)
         {
             int n = portfolios.Length;
             if (n < 2) {
